faa72ae9c90c972da4ee181d01a65ab725a06bf2
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2018-01-21 16:13:47 +0200

    install  codeception add code metrics tools

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Goutte\Client;
 
 class HomeController extends Controller
 {
+    /**
+     *
+     */
     public function index()
     {
-
         $client = new Client();
         $crawler = $client->request('GET', 'http://www.vsetv.com/schedule_printversion_withdesc.html');
-        $crawler->filter('.prname2')->each(function ($node) {
-            print $node->text()."\n";
-        });
+        $result = '';
 
-        return "sdfdfd";
+        $crawler->filter('.prname2')->each(
+            function ($node, $result) {
+                $result .= $node->text() . "\n";
+            }
+        );
 
+        return $result;
     }
 }
