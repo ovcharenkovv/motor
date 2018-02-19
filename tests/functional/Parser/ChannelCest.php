4deb6a57d51deea5ff1e963ef3c1aaa8bc8b0346
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2018-02-19 20:14:02 +0200

    add channel ids parser fix test name

--- tests/functional/Parser/ChannelCest.php
+++ tests/functional/Parser/ChannelCest.php
@@ -2,13 +2,12 @@
 namespace Parse;
 
 use App\Vsetv\Parser\Channels;
-use App\Vsetv\Parser\Time;
 use FunctionalTester as Tester;
 use Symfony\Component\DomCrawler\Crawler;
 
 class ChannelCest
 {
-    public function testExtractHour(Tester $I)
+    public function testExtractChannelNameAndId(Tester $I)
     {
         $html = '<select name="selected_channel"><option value="val">text</option></select>';
 
