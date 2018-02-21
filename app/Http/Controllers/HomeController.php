30d525e3febf05651e37cef051b450f0a67d381f
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2018-02-21 22:06:45 +0200

    display all channels

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Goutte\Client;
-use Symfony\Component\DomCrawler\Crawler;
+use App\Models\ScrapChannel;
 use Illuminate\Contracts\Cache\Repository as CacheManager;
 
 class HomeController extends Controller
@@ -11,22 +10,8 @@ class HomeController extends Controller
 
     public function index(CacheManager $cache)
     {
-        if ($cache->has('sony_html')) {
-            $html = $cache->get('sony_html');
-        } else {
-            $client = new Client();
-            $html = $client->request('GET', 'http://www.vsetv.com/schedule_channel_403_week.html')->html();
-
-            $expiresAt = now()->addMinutes(10);
-            $cache->put('sony_html', $html, $expiresAt);
-        }
-
-        $crawler = new Crawler();
-        $crawler->addHtmlContent($html);
-
-        $crawler->filter('.time img')->each(function ($node) {
-            print $node->attr('src')."<br>";
-        });
+        echo "<pre>";
+        print_r(ScrapChannel::all()->toArray());
 
         return 'Hello world !!!';
     }
